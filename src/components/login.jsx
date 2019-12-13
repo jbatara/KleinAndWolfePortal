89c4204a592dad5f4e6c4eb410a202914706ca96
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-13 11:38:20 -0800

    Change Login from class to function

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
-class Login extends (props)
-{
-  return(
+function Login(props) {
+  return (
+    <div className='splash block'>
+      <h1>Login</h1>
+      <form onSubmit={submit}>
+        <tr>
+          <th><label for="Email">Email</label></th>
+          <th><input type="text" id='email' /></th>
+        </tr>
+        <tr>
+          <th><label for="Password">Password</label></th>
+          <th><input type='password' id='password' /></th>
+        </tr>
 
-      <div class='splash block'>
-        <h1>Login</h1>
-        <form onSubmit={submit}>
-          <tr>
-            <th><label for="Email">Email</label></th>
-            <th><input type="text" id='email' /></th>
-          </tr>
-          <tr>
-            <th><label for="Password">Password</label></th>
-            <th><input type='password' id='password' /></th>
-          </tr>
-          
-          <button type='submit' class='btn btn-light right'>Login</button>
-        </form>
-      </div>
+        <button type='submit' class='btn btn-light right'>Login</button>
+      </form>
+    </div>
   );
 }
 
