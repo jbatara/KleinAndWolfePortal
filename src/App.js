abce238f52426c3cebf4a29a93586d7a375d1d4a
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-13 08:19:54 -0800

    Add switch to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {
+  Switch,
+  Route
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className=" App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +22,48 @@ function App() {
         >
           Learn React
         </a>
+        <Switch>
+          <Route exact path='/'
+            component={splashPage} />
+
+          <Route exact path='/menu'
+            render={() =>
+              <AllKegsPatron
+                kegs={this.state.kegs}
+                onDecrementPint={this.handleDecrementPint} />} />
+
+          <Route exact path='/kegs'
+            render={() => {
+              <AllKegsEmployee
+                kegs={this.state.kegs}
+                loginState={this.state.loginState}/>;
+            }} />
+
+          <Route exact path='/employee/login'
+            render={() => (
+              <EmployeeLoginControl
+                loginState={this.state.loginState}
+                currentUser={this.state.currentUser}
+                onLoginClick={this.handleLoginClick} />
+            )} />
+          {/* <Route exact path='/employee/login' render={() => (<EmployeeLoginForm />)} /> */}
+
+          <Route path='/keg/new'
+            render={() => {
+              <NewKegControl
+                kegs={this.state.kegs}
+                loginState={this.state.loginState}
+                onAddNewKeg={this.handleAddNewKeg} />;
+            }} />
+
+          <Route path='/keg/edit/'
+            render={() => {
+              <EditKegControl
+                kegs={this.state.kegs}
+                loginState={this.state.loginState}
+                onEditKeg={this.handleEditKeg} />;
+            }} />
+        </Switch>
       </header>
     </div>
   );
