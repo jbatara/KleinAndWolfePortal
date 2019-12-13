bc57980418c3b6e1752ed4ac23f72049c4fe6aa1
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-13 10:30:45 -0800

    Change routes to include only login.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  Switch,
-  Route
-} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login'
@@ -11,59 +8,9 @@ function App() {
   return (
     <div className="App">
       <header className=" App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Switch>
           <Route exact path='/'
             component={Login} />
-
-          <Route exact path='/menu'
-            render={() =>
-              <AllKegsPatron
-                kegs={this.state.kegs}
-                onDecrementPint={this.handleDecrementPint} />} />
-
-          <Route exact path='/kegs'
-            render={() => {
-              <AllKegsEmployee
-                kegs={this.state.kegs}
-                loginState={this.state.loginState}/>;
-            }} />
-
-          <Route exact path='/employee/login'
-            render={() => (
-              <EmployeeLoginControl
-                loginState={this.state.loginState}
-                currentUser={this.state.currentUser}
-                onLoginClick={this.handleLoginClick} />
-            )} />
-          {/* <Route exact path='/employee/login' render={() => (<EmployeeLoginForm />)} /> */}
-
-          <Route path='/keg/new'
-            render={() => {
-              <NewKegControl
-                kegs={this.state.kegs}
-                loginState={this.state.loginState}
-                onAddNewKeg={this.handleAddNewKeg} />;
-            }} />
-
-          <Route path='/keg/edit/'
-            render={() => {
-              <EditKegControl
-                kegs={this.state.kegs}
-                loginState={this.state.loginState}
-                onEditKeg={this.handleEditKeg} />;
-            }} />
         </Switch>
       </header>
     </div>
