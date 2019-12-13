85643d65863eab61b3c40db09dc3563c89f5d88e
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-13 15:12:26 -0800

    Modify to incorporate redux and store

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/login'
+import { createStore, applyMiddleware } from 'redux';
+import Login from './components/Login'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className=" App-header">
-        <Switch>
-          <Route exact path='/'
-            component={Login} />
-        </Switch>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path='/'
+          component={Login} />
+        <Route exact path='/database'
+          component={Database} />
+        <Route exact path='/charts'
+          component={Charts} />
+      </Switch>
     </div>
   );
+
 }
 
 export default App;
