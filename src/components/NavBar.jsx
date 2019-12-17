65975bd5a3cda879046db968bb9cb9094a85b7af
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-17 15:54:01 -0800

    Change links on navbar to reflect client user view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 function NavBar(props) {
 
@@ -13,10 +14,7 @@ function NavBar(props) {
             <a className="nav-link" href="#/">Home<span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/database">Database<span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/charts">Charts</a>
+            <a className="nav-link" href="#/livefeedtable">Live Feed <span className="sr-only">(current)</span></a>
           </li>
         </ul>
       </div>
@@ -24,4 +22,4 @@ function NavBar(props) {
   return navbar;
 }
 
-export default NavBar;
+export default connect()(NavBar);
