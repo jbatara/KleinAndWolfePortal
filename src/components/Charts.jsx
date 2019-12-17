25143cbc5939ec6d3cf10792aa2afc4580de92b4
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-17 15:50:39 -0800

    Add call to nivo package to create mock bar chart

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,53 +1,10 @@
 import React from 'react';
 import Chart from 'chart.js';
-import { ResponsiveCalendarCanvas } from '@nivo/calendar'
-
-//this is an example of nivo calendar
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
-const MyResponsiveCalendarCanvas = ({ data /* see data tab */ }) => (
-  <ResponsiveCalendarCanvas
-    data={data}
-    from="2013-03-01"
-    to="2019-07-12"
-    emptyColor="#aa7942"
-    colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
-    margin={{ top: 40, right: 40, bottom: 50, left: 40 }}
-    direction="vertical"
-    monthBorderColor="#ffffff"
-    dayBorderWidth={0}
-    dayBorderColor="#ffffff"
-    legends={[
-      {
-        anchor: 'bottom-right',
-        direction: 'row',
-        translateY: 36,
-        itemCount: 4,
-        itemWidth: 42,
-        itemHeight: 36,
-        itemsSpacing: 14,
-        itemDirection: 'right-to-left'
-      }
-    ]}
-  />
-)
-
-function Charts(){
-
-  return (
-    <React.Fragment>
-      <canvas id="myChart" width="400" height="400"></canvas>
-    </React.Fragment>
-    
-  );
-}
+import {ResponsiveBarCanvas} from '@nivo/bar';
 
 //this is an example of chart.js
-function barChart(canvasId){
-  var ctx = document.getElementById(canvasId);
+function barChart(chartRef) {
+  var ctx = chartRef.current;
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -86,5 +43,132 @@ function barChart(canvasId){
   });
   return myChart;
 }
+
+class Charts extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.chartRef = React.createRef();
+    this.state = { type: props.type, data: props.data };
+  }
+  componentDidMount() {
+    if (this.state.type === 'bar') {
+      barChart(this.chartRef);
+    }
+  }
+  
+  render() {
+    var style = { height: 500 };
+    return (
+      <div style={style}>
+        <canvas ref={this.chartRef} width={'400 px'} height={'400 px'} />
+        {MyResponsiveBarCanvas(this.state.data)}
+      </div>
+
+    );
+  }
+}
+const MyResponsiveBarCanvas = (data) => (
+  <ResponsiveBarCanvas
+    data={data}
+    keys={[
+      'hot dog',
+      'burger',
+      'sandwich',
+      'kebab',
+      'fries',
+      'donut',
+      'junk',
+      'sushi',
+      'ramen',
+      'curry',
+      'udon',
+      'bagel',
+      'yakitori',
+      'takoyaki',
+      'tacos',
+      'miso soup',
+      'tortilla',
+      'tapas',
+      'chipirones',
+      'gazpacho',
+      'soba',
+      'bavette',
+      'steak',
+      'pizza',
+      'spaghetti',
+      'ravioli',
+      'salad',
+      'pad thai',
+      'bun',
+      'waffle',
+      'crepe',
+      'churros',
+      'paella',
+      'empanadas',
+      'bruschetta',
+      'onion soup',
+      'cassoulet',
+      'bouillabaisse',
+      'unagi',
+      'tempura',
+      'tonkatsu',
+      'shabu-shabu',
+      'twinkies',
+      'jerky',
+      'fajitas',
+      'jambalaya',
+      'meatloaf',
+      'mac n cheese',
+      'baked beans',
+      'popcorn',
+      'buffalo wings',
+      'BBQ ribs',
+      'apple pie',
+      'nachos',
+      'risotto',
+      'tiramisu'
+    ]}
+    indexBy="country"
+    margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
+    pixelRatio={2}
+    padding={0.15}
+    innerPadding={0}
+    minValue="auto"
+    maxValue="auto"
+    groupMode="stacked"
+    layout="horizontal"
+    reverse={false}
+    colors={{ scheme: 'red_blue' }}
+    colorBy="id"
+    borderWidth={0}
+    borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+    axisTop={{ tickSize: 5, tickPadding: 5, tickRotation: 0, legend: '', legendOffset: 36 }}
+    axisRight={null}
+    axisBottom={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'country',
+      legendPosition: 'middle',
+      legendOffset: 36
+    }}
+    axisLeft={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'food',
+      legendPosition: 'middle',
+      legendOffset: -40
+    }}
+    enableGridX={true}
+    enableGridY={false}
+    enableLabel={true}
+    labelSkipWidth={12}
+    labelSkipHeight={12}
+    labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+    isInteractive={true}
+  />
+)
 
 export default Charts;
