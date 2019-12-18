826f0c3bc7ed799df3f49a002e8bb95970337d61
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-17 16:17:37 -0800

    Create store by calling configure store and create browser history using base tag

--- src/index.js
+++ src/index.js
@@ -7,9 +7,18 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './reducers/index';
 import {HashRouter} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import configureStore from './store/configureStore';
 
-const store = createStore(rootReducer);
+// Create browser history to use in the Redux store
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const history = createBrowserHistory({
+  basename: baseUrl
+});
 
+// Get the application-wide store instance, prepopulating with state from the server where available.
+const initialState = window.initialReduxState;
+const store = configureStore(history, initialState);
 
 ReactDOM.render(
   <Provider store={store}>
