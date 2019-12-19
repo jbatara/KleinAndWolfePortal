3970264a9ea4f3d329f0c8655b25a5c65075be25
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-18 23:54:05 -0800

    WIP Add Nivo ResponsiveGeoMapCanvas to Traffic
    GeoMap does not render map of world, only grid

--- src/components/Traffic.jsx
+++ src/components/Traffic.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import { ResponsiveGeoMapCanvas } from '@nivo/geo';
+import {filterByDate, } from '../constants/DataManipulation';
 
 function Traffic(props){
-  return null;
+  var style = {height: '500px'}
+  return (
+    <div className='container' style={style}>
+      {MyResponsiveGeoMap(null)}
+    </div>
+  );
 }
 
-export default Traffic;
+const MyResponsiveGeoMap = (data) => (
+  <ResponsiveGeoMapCanvas
+    features={[{
+      "location": {
+        "latitude": 41.26426747188511,
+        "longitude": 80.65774637884769}}]}
+    margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+    projectionTranslation={[0.5, 0.5]}
+    projectionRotation={[0, 0, 0]}
+    fillColor="#eeeeee"
+    borderWidth={0.5}
+    borderColor="#333333"
+    enableGraticule={true}
+    graticuleLineColor="#666666"
+  />
+)
+const mapPropsToState = state =>{
+  return{
+    traffic: state.traffic.data
+  };
+}
+
+export default connect(mapPropsToState)(Traffic);
