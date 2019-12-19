5e6eeeb7fa4de8315338038eb1ffd54e7c8d1615
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-19 00:38:55 -0800

    WIP Orders

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,7 +1,63 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as DataManipulation from '../constants/DataManipulation';
+var moment = require('moment');
 
 function Orders(props) {
-  return null;
+  const filteredData = DataManipulation.filterByDate('w', 2018, props.week, props.orders);
+  const tableBody = filteredData.map((order) => {
+    return (<tr>
+      <td>{order.id}</td>
+      <td>{moment(order.date).format('ddd MM/DD/YYYY')}</td>
+      <td>{moment(order.date).format('HH:mm')}</td>
+      <td>{'$' + order.subtotal.toFixed(2).toString()}</td>
+      <td>{printCart(order.cart).map}</td>
+      <td><p>{'Lat: ' + order.origination.latitude}</p><p>{'Lon: ' + order.origination.longitude}</p></td>
+      <td>{order.customer.id}</td>
+    </tr>);
+  });
+  return (
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID#</th>
+          <th scope="col">Date</th>
+          <th scope="col">Time</th>
+          <th scope="col">Subtotal ($USD)</th>
+          <th scope="col">Cart</th>
+          <th scope="col">Location</th>
+          <th scope="col">CustomerID</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          {tableBody}
+        }
+      </tbody>
+    </table>
+  );
 }
 
-export default Orders;
+
+
+function printCart(cart) {
+  let output = [];
+  for (let i = 0; i < cart.length; i++) {
+    const item = cart[i];
+    output.push(
+      <p>{item.id.toString() + ": " + item.name + ' (' + item.roast + ", " + item.origin + ") " + item.qty.toString() + " units x $" + item.price.toFixed(2).toString()}</p>
+    );
+  }
+  return output;
+}
+
+const mapStateToProps = state => {
+  return {
+    orders: state.orders.data,
+    orderMeta: state.orders.meta,
+    week: 45,
+    products: state.products.data
+  };
+};
+
+export default connect(mapStateToProps)(Orders);
