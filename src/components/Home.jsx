c4ed0587f01ad18cd03cfbe03d980516e2fa4ab6
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-18 23:24:33 -0800

    Add home page to display stream health

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 function Home(props){
-  return null;
+  return (
+    <React.Fragment>
+      <h1>Welcome, {props.user.userName}.</h1>
+      <table className='table table-hover'>
+        <tr>
+          <td><h3>Last Product Fetch: </h3></td>
+          <td><h3>{props.productMeta}</h3></td>
+        </tr>
+        <tr>
+          <td><h3>Last Order Fetch: </h3></td>
+          <td><h3>{props.orderMeta}</h3></td>
+        </tr>
+        <tr>
+          <td><h3>Last Traffic Fetch: </h3></td>
+          <td><h3>{props.trafficMeta}</h3></td>
+        </tr>
+      </table>
+    </React.Fragment>
+  );
 }
 
-export default Home;
+const mapStateToProps = state =>{
+  return {
+    productMeta: state.products.meta,
+    orderMeta: state.orders.meta,
+    trafficMeta: state.traffic.meta,
+    user: state.user
+  }
+};
+
+export default connect(mapStateToProps)(Home);
