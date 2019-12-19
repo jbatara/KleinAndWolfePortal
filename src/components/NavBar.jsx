d5c6d18586b5aee13d1d7e20a7de0994614452dc
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-18 22:26:17 -0800

    Change links in nav bar to new organization

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,18 +4,25 @@ import {connect} from 'react-redux';
 function NavBar(props) {
 
    let  navbar = <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+     <a className="navbar-brand" href="#/">Klein & Wolfe</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <a className="nav-link" href="#/">Home<span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/livefeedtable">Live Feed <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="#/analytics">Analytics<span className="sr-only"></span></a>
           </li>
+         <li className="nav-item">
+           <a className="nav-link" href="#/traffic">Traffic <span className="sr-only"></span></a>
+         </li>
+         <li className="nav-item">
+           <a className="nav-link" href="#/orders">Orders <span className="sr-only"></span></a>
+         </li>
         </ul>
       </div>
     </nav>;
