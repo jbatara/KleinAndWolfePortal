e95c67479c999df5d4eb1b1afa5d6cfee5d0ebce
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-18 22:28:51 -0800

    Add @nivo/bar charts conditionally rendered into styled lifedataview

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,135 +1,24 @@
 import React from 'react';
-import Chart from 'chart.js';
-import {ResponsiveBarCanvas} from '@nivo/bar';
+import { ResponsiveBarCanvas } from '@nivo/bar';
+import * as DataManipulation from './../constants/DataManipulation';
 
-//this is an example of chart.js
-function barChart(chartRef) {
-  var ctx = chartRef.current;
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
-  });
-  return myChart;
-}
+function Charts(props) {
 
-class Charts extends React.Component {
+  var style = { height: '50vh', width: '50vw', borderWidth: '1px', borderColor:'grey'};
+  return (
+    <div className='col' style={style}>
+      <h2></h2>
+      {chartTitle(props.type, props.weekNumber)}
+      {chartByType(props.type, props.data.orders, props.weekNumber, props.products.products)}
+    </div>
 
-  constructor(props) {
-    super(props);
-    this.chartRef = React.createRef();
-    this.state = { type: props.type, data: props.data };
-  }
-  componentDidMount() {
-    if (this.state.type === 'bar') {
-      barChart(this.chartRef);
-    }
-  }
-  
-  render() {
-    var style = { height: 500 };
-    return (
-      <div style={style}>
-        <canvas ref={this.chartRef} width={'400 px'} height={'400 px'} />
-        {MyResponsiveBarCanvas(this.state.data)}
-      </div>
-
-    );
-  }
+  );
 }
 const MyResponsiveBarCanvas = (data) => (
   <ResponsiveBarCanvas
-    data={data}
-    keys={[
-      'hot dog',
-      'burger',
-      'sandwich',
-      'kebab',
-      'fries',
-      'donut',
-      'junk',
-      'sushi',
-      'ramen',
-      'curry',
-      'udon',
-      'bagel',
-      'yakitori',
-      'takoyaki',
-      'tacos',
-      'miso soup',
-      'tortilla',
-      'tapas',
-      'chipirones',
-      'gazpacho',
-      'soba',
-      'bavette',
-      'steak',
-      'pizza',
-      'spaghetti',
-      'ravioli',
-      'salad',
-      'pad thai',
-      'bun',
-      'waffle',
-      'crepe',
-      'churros',
-      'paella',
-      'empanadas',
-      'bruschetta',
-      'onion soup',
-      'cassoulet',
-      'bouillabaisse',
-      'unagi',
-      'tempura',
-      'tonkatsu',
-      'shabu-shabu',
-      'twinkies',
-      'jerky',
-      'fajitas',
-      'jambalaya',
-      'meatloaf',
-      'mac n cheese',
-      'baked beans',
-      'popcorn',
-      'buffalo wings',
-      'BBQ ribs',
-      'apple pie',
-      'nachos',
-      'risotto',
-      'tiramisu'
-    ]}
-    indexBy="country"
+    data={data.data}
+    keys={data.keys}
+    indexBy={data.indexBy}
     margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
     pixelRatio={2}
     padding={0.15}
@@ -137,31 +26,49 @@ const MyResponsiveBarCanvas = (data) => (
     minValue="auto"
     maxValue="auto"
     groupMode="stacked"
-    layout="horizontal"
+    layout="vertical"
     reverse={false}
-    colors={{ scheme: 'red_blue' }}
+    colors={{ scheme: 'green_blue' }}
     colorBy="id"
     borderWidth={0}
-    borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-    axisTop={{ tickSize: 5, tickPadding: 5, tickRotation: 0, legend: '', legendOffset: 36 }}
+    borderColor={{ from: '#808080' }}
+    axisTop={null}
     axisRight={null}
-    axisBottom={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: 'country',
-      legendPosition: 'middle',
-      legendOffset: 36
-    }}
+    axisBottom={null}
     axisLeft={{
       tickSize: 5,
       tickPadding: 5,
       tickRotation: 0,
-      legend: 'food',
+      legend: data.axisLeftLegend,
       legendPosition: 'middle',
       legendOffset: -40
     }}
-    enableGridX={true}
+    legends={[
+      {
+        dataFrom: 'keys',
+        anchor: 'bottom-right',
+        direction: 'column',
+        justify: false,
+        translateX: 0,
+        translateY: 0,
+        itemsSpacing: 2,
+        itemWidth: 30,
+        itemHeight: 20,
+        itemDirection: 'left-to-right',
+        itemOpacity: 0.85,
+        symbolSize: 20,
+        effects: [
+          {
+            on: 'hover',
+            style: {
+              itemOpacity: 1
+            }
+          }
+        ]
+      }
+    ]}
+    tooltip={data.tooltip}
+    enableGridX={false}
     enableGridY={false}
     enableLabel={true}
     labelSkipWidth={12}
@@ -169,6 +76,29 @@ const MyResponsiveBarCanvas = (data) => (
     labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
     isInteractive={true}
   />
-)
+);
+
+function chartTitle(type, weekNumber) {
+  switch (type) {
+    case 'product_rev_week':
+      return (<h4><em>Top 5 Products by Revenue: Week {weekNumber}</em></h4>);
+    case 'product_qty_week':
+      return (<h4><em>Top 5 Products by Units Sold: Week {weekNumber}</em></h4>);
+    default:
+      return (<h3>chartTitle Error</h3>);
+  }
+}
+function chartByType(type, data, weekNumber, productsArray) {
+  switch (type) {
+    case 'product_rev_week':
+      return MyResponsiveBarCanvas(
+        DataManipulation.topFiveProductsByWeek_Revenue(data, weekNumber, 'product', productsArray));
+    case 'product_qty_week':
+      return MyResponsiveBarCanvas(
+        DataManipulation.topFiveProductsByWeek_Qty(data, weekNumber, 'product', productsArray));
+    default:
+      return <p>That is an invalid chart type.</p>;
+  }
+}
 
 export default Charts;
