57f199307757695785b509504528057ddbad156e
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-18 23:01:03 -0800

    Delete unnecessary data examples for chart input

--- src/components/LiveDataView.jsx
+++ src/components/LiveDataView.jsx
@@ -3,65 +3,6 @@ import { connect } from 'react-redux';
 import Charts from './Charts';
 
 function LiveDataView(props) {
-
-  const height = 100;
-  var style = { height: 500 };
-
-  let dataArray3 = [
-    {
-      "food": "hot dog",
-      "hot dog": 154,
-      "hot dogColor": "hsl(43, 70%, 50%)"
-    },
-    {
-      "food": "burger",
-      "burger": 120,
-      "burgerColor": "hsl(279, 70%, 50%)"
-    },
-    {
-      "food": "sandwich",
-      "sandwich": 129,
-      "sandwichColor": "hsl(215, 70%, 50%)"
-    },
-    {
-      "food": "kebab",
-      "kebab": 196,
-      "kebabColor": "hsl(79, 70%, 50%)"
-
-    },
-    {
-      "food": "fries",
-      "fries": 127,
-      "friesColor": "hsl(156, 70%, 50%)"
-    },
-    {
-      "food": "donut",
-      "donut": 188,
-      "donutColor": "hsl(138, 70%, 50%)"
-    }
-  ];
-
-  let data4 = {
-    keys:
-      ['Harar Highlands',
-        'Kopi Tubruk',
-        'Cinnamon Roast',
-        'Morning Cup',
-        'Nairobi Explorer',
-        'Original Espresso',
-      ],
-    data:
-      [{ product: 'Harar Highlands', 'Harar Highlands': 136, 'Harar HighlandsColor': 'hsl(52, 70%, 50%)' },
-        { product: 'Kopi Tubruk', 'Kopi Tubruk': 89, 'Kopi TubrukColor': 'hsl(52, 70 %, 50 %)' },
-      { product: 'Cinnamon Roast', 'Cinnamon Roast': 88 },
-      { product: 'Morning Cup', 'Morning Cup': 56 },
-      { product: 'Nairobi Explorer', 'Nairobi Explorer': 19 },
-      { product: 'Original Espresso', 'Original Espresso': 0 }],
-    axisBottomLegend: 'Days',
-    axisLeftLegend: 'Quantity',
-    indexBy: 'product'
-  }
-
   return (
     <div className='flex-grid'>
       <Charts type='product_rev_week' data={props.orders} weekNumber={45} products={props.products}/>
@@ -70,13 +11,11 @@ function LiveDataView(props) {
   );
 }
 
-
 const mapStateToProps = state => {
   return {
     orders: state.orders,
     products: state.products
   }
 }
-
 
 export default connect(mapStateToProps)(LiveDataView);
